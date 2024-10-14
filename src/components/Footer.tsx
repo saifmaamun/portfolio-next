e700b4264dfb174aa6be16e75a5645d8fdf81e52
Saif Maamun <sadifmamun@gmail.com>
2024-10-14 11:05:20 +0600

    Fotter [DONE]

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -221,7 +221,7 @@ const Footer: React.FC = () => {
           <div className="flex-col space-y-2 align-middle it">
             {/* git */}
             <a
-              href="https://github.com/your-username"
+              href="https://github.com/saifmaamun"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center space-x-4  transition-all duration-300"
@@ -302,11 +302,11 @@ const Footer: React.FC = () => {
 	C120.272,385.011,123.256,388.507,126.368,391.387z"
                 />
               </svg>
-              <p>github</p>
+              <p>@saifmaamun</p>
             </a>
             {/* linkedin */}
             <a
-              href="https://linkedin.com/in/your-username"
+              href="https://www.linkedin.com/in/saif-maamun/"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center space-x-4 hover:text-glowinglime transition-all duration-300"
@@ -324,14 +324,14 @@ const Footer: React.FC = () => {
                   d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"
                 />
               </svg>
-              <p>linkedIn</p>
+              <p>@saif-maamun</p>
             </a>
             {/* x */}
             <a
-              href="https://twitter.com/your-username"
+              href="https://x.com/imsaifmaamun"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center space-x-2 hover:text-glowinglime transition-all duration-300"
+              className="flex items-center space-x-4 hover:text-glowinglime transition-all duration-300"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -392,13 +392,14 @@ const Footer: React.FC = () => {
                   points="24.45,21.721 25.355,23.01 33.136,14 31.136,14"
                 ></polygon>
               </svg>
+              <p> @imsaifmaamun</p>
             </a>
             {/* instagram*/}
             <a
-              href="https://twitter.com/your-username"
+              href="https://www.instagram.com/desprdo/"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center space-x-2 hover:text-glowinglime transition-all duration-300"
+              className="flex items-center space-x-4 hover:text-glowinglime transition-all duration-300"
             >
               <svg
                 width="800px"
@@ -488,13 +489,14 @@ const Footer: React.FC = () => {
                   </radialGradient>
                 </defs>
               </svg>
+              <p>@desprdo</p>
             </a>
             {/* facebook*/}
             <a
-              href="https://twitter.com/your-username"
+              href="https://www.facebook.com/maamunsaif/"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center space-x-2 hover:text-glowinglime transition-all duration-300"
+              className="flex items-center space-x-4 hover:text-glowinglime transition-all duration-300"
             >
               <svg
                 width="800px"
@@ -549,6 +551,7 @@ const Footer: React.FC = () => {
                   />
                 </g>
               </svg>
+              <p>@maamunsaif</p>
             </a>
           </div>
         </div>
