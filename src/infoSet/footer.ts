228b9083ad24ec656a98f1963691b8f50f6d2e78
Saif Maamun <sadifmamun@gmail.com>
2024-10-17 08:20:46 +0600

    footer [DONE]

---
+++ src/infoSet/footer.ts
@@ -0,0 +1,58 @@
+import mail from "../assets/gmail-svgrepo-com.svg";
+import phone from "../assets/phone-svgrepo-com.svg";
+import whatsapp from "../assets/whatsapp-svgrepo-com.svg";
+import address from "../assets/location-svgrepo-com.svg";
+import git from "../assets/github-svgrepo-com.svg";
+import linkedin from "../assets/linkedin-svgrepo-com.svg";
+import insta from "../assets/instagram-1-svgrepo-com.svg";
+import fb from "../assets/facebook-media-network-svgrepo-com.svg";
+import x from "../assets/x.svg";
+export const socials = [
+  {
+    icon: git,
+    handle: "@saifmaamun",
+    href: "https://github.com/saifmaamun",
+  },
+  {
+    icon: linkedin,
+    handle: "@saifmaamun",
+    href: "https://github.com/saifmaamun",
+  },
+  {
+    icon: insta,
+    handle: "@saifmaamun",
+    href: "https://github.com/saifmaamun",
+  },
+  {
+    icon: fb,
+    handle: "@saifmaamun",
+    href: "https://github.com/saifmaamun",
+  },
+  {
+    icon: x,
+    handle: "@saifmaamun",
+    href: "https://github.com/saifmaamun",
+  },
+];
+export const contacts = [
+  {
+    src: mail,
+    value: "saifmaamun@gmail.com",
+    href: "mailto:saifmaamun@gmail.com",
+  },
+  {
+    src: phone,
+    value: "+8801305797595",
+    href: "tel:+8801305797595",
+  },
+  {
+    src: whatsapp,
+    value: "+8801305797595",
+    href: "tel:+8801305797595",
+  },
+  {
+    src: address,
+    value: "Siddhirgonj, Narayangonj, Dhaka,Bangladesh",
+    href: "",
+  },
+];
